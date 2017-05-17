3426d74094a02c793cd768f42408cf95e718c92c
Jorge Abad <jorgejoseabad@gmail.com>
2017-05-17 18:35:41 +0200

    4 iteration

--- starter-code/lab-movies/routes/celebrities.js
+++ starter-code/lab-movies/routes/celebrities.js
@@ -3,6 +3,17 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require('../models/celebrity');
 
+router.post('/',(req,res,next)=>{
+  const dataCeleb=req.body;
+  const newCeleb = new Celebrity(dataCeleb);
+  console.log(newCeleb);
+  newCeleb.save(function(err,doc){
+    if (err) return next(err);
+    res.redirect('/celebrities');
+  });
+});
+
+
 router.get('/',(req,res,next)=>{
   //res.send('Route of celebrities');
   Celebrity.find({},(error,celebs)=>{
@@ -12,8 +23,19 @@ router.get('/',(req,res,next)=>{
 
 });
 
+
+
+
+router.get('/new',(req,res,next)=>{
+  console.log('in router get new');
+  //res.send('intento añadir uno nuevo');
+  res.render('celebrities/new');
+});
+
+
 router.get('/:id',(req,res,next)=>{
   const idC=req.params.id;
+  console.log('we are in /celebrities/:id');
   console.log(idC);
   //res.send(idC);
   Celebrity.findById(idC,(error,celeb)=>{
@@ -21,6 +43,7 @@ router.get('/:id',(req,res,next)=>{
     res.render('celebrities/show',{celeb});
   });
 });
+
 
 
 module.exports = router;
