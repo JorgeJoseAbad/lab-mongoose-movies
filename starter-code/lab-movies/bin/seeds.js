76903a5e97c9d0d6fa88d17cb7806372cb75bb26
Jorge Abad <jorgejoseabad@gmail.com>
2017-05-17 21:01:02 +0200

    iteration 7 Movies model

--- starter-code/lab-movies/bin/seeds.js
+++ starter-code/lab-movies/bin/seeds.js
@@ -1,19 +1,26 @@
 /* jshint esversion:6 */
-const celebrityData=[
+/*const celebrityData=[
   {name:'Julio Iglesias',occupation:'Singer',catchPhrase:'ey, no puedo estar sin ti..'},
   {name:'Cristiano Ronaldo',occupation:'football player',catchPhrase:'Im Gold balloon'},
   {name:'Arnold Swartzeneger',occupation:'actor',catchPhrase:'Sayonara baby'}
+];*/
+
+const movieData=[
+{title:'A bridge to far', genre:'belic', plot:'historic about operation Market Garden'},
+{title:'Ivanhoe', genre:'Historic', plot:'A hero in the two roses war'},
+{title:'Yo hice a Roque III', genre:'comedia', plot:'un par de granujas imitan a Stallone'}
+
 ];
 
 // bin/seeds.js
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/celebrities');
-const Celebrity = require('../models/celebrity');
+const Movie = require('../models/movie');
 
 // bin/seeds.js
 //from the nongoose API: Model.create(doc(s), [callback])
 //Shortcut for saving one or more documents to the database.
-Celebrity.create(celebrityData, (err, docs) => {
+Movie.create(movieData, (err, docs) => {
   if (err) { throw err; }
   docs.forEach( (docs) => {
     console.log(docs.name);
